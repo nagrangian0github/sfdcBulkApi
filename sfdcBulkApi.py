970b1927627b9266e56783f61e395d3afd6fa4e9
Your Name <you@example.com>
2021-12-04 16:42:12 +0900

    Class化

--- sfdcBulkApi.py
+++ sfdcBulkApi.py
@@ -6,118 +6,146 @@ import requests
 import configparser
 
 
-version_number = "v53.0"
-
-
 def main():
     print("-----Salesforce Bulk API 2.0------")
-
-    instance_url, access_token = get_oauth()
-    jobid, state = post_job(instance_url, access_token)
-    state = get_querybob(jobid, instance_url, access_token)
+    sp = SalsforceApi()
+    sp.post_job()
+    state = sp.get_querybob()
     while state in ["UploadComplete", "InProgress"]:
-        state = get_querybob(jobid, instance_url, access_token)
-        print(state)
-    f = open('results.csv', 'w', encoding='UTF-8')
-    f.write(get_queryresults(jobid, instance_url, access_token))
-    f.close()
+        state = sp.get_querybob()
+    print('Job State：' + state)
+    sp.get_queryresults()
+    sp.write_results()
+    if sp.delete_queryjob() == 204:
+        print('Job Delete： Success')
 
 
-def get_queryresults(jobid, instance_url, access_token):
-    authorization = 'Bearer ' + access_token
-    headers = {'Accept': 'text/csv',
-               'Authorization': authorization
-               }
-    url_queryjob = instance_url + "/services/data/" + version_number + "/jobs/query/" + jobid + "/results"
-    res = requests.get(url_queryjob, headers=headers)
-    res.encoding = res.apparent_encoding
-    # 実行結果を判定
-    if res.status_code != 200:
-        raise Exception(f'code:{res.status_code}, text:{res.text}')
+class SalsforceApi:
 
-    # 実行結果の取得
-    res_info = res.text
-    return res_info
+    def __init__(self):
+        self.jobid = ''
+        self.results = ''
 
+        # --------------------------------------------------
+        # configparserの宣言とiniファイルの読み込み
+        # --------------------------------------------------
+        config_ini = configparser.ConfigParser()
+        config_ini.read('config.ini', encoding='utf-8')
 
-def get_querybob(jobid, instance_url, access_token):
-    authorization = 'Bearer ' + access_token
-    headers = {'content-type': 'application/json',
-               'Authorization': authorization
-               }
-    url_queryjob = instance_url + "/services/data/" + version_number + "/jobs/query/" + jobid
+        my_domain = config_ini['DEFAULT']['my_domain']
+        url_auth = "https://" + my_domain + ".my.salesforce.com/services/oauth2/token"
+        client_id = config_ini['DEFAULT']['client_id']
+        client_secret = config_ini['DEFAULT']['client_secret']
+        username = config_ini['DEFAULT']['username']
+        pw = config_ini['DEFAULT']['pw']
+        self.soql = config_ini['DEFAULT']['soql']
+        self.version_number = config_ini['DEFAULT']['version_number']
+        self.output_file = config_ini['DEFAULT']['output_file']
 
-    res = requests.get(url_queryjob, headers=headers)
-    # 実行結果を判定
-    if res.status_code != 200:
-        raise Exception(f'code:{res.status_code}, text:{res.text}')
+        # SalesForceへのセッション情報を取得
+        headers = {'content-type': 'application/x-www-form-urlencoded'}
+        payload = {
+            "grant_type": "password",
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "username": username,
+            "password": pw
+        }
+        res = requests.post(url_auth, headers=headers, data=payload)
 
-    # 実行結果の取得
-    res_info = json.loads(res.text)
-    state = res_info['state']
-    return state
+        # 実行結果を判定
+        if res.status_code != 200:
+            raise Exception(f'code:{res.status_code}, text:{res.text}')
 
+        # 実行結果の取得
+        res_info = json.loads(res.text)
+        self.instance_url = res_info['instance_url']
+        self.access_token = res_info['access_token']
 
-def post_job(instance_url, access_token):
-    authorization = 'Bearer ' + access_token
-    headers = {'content-type': 'application/json',
-               'Authorization': authorization
-               }
-    payload = {
-                "operation": "query",
-                "query": "SELECT Id, Name, Description FROM Account",
-                "contentType": "CSV",
-                "columnDelimiter": "COMMA",
-                "lineEnding": "CRLF"
-    }
-    url_query = instance_url + "/services/data/" + version_number + "/jobs/query"
+        print('接続ID：' + username)
+        print('API Version：' + self.version_number)
+        print('instance_url：' + self.instance_url)
 
-    res = requests.post(url_query, headers=headers, data=json.dumps(payload))
-    # 実行結果を判定
-    if res.status_code != 200:
-        raise Exception(f'code:{res.status_code}, text:{res.text}')
+    def write_results(self):
+        f = open(self.output_file, 'w', newline='\n', encoding='UTF-8')
+        f.write(self.results)
+        f.close()
+        print('Output File：' + self.output_file)
 
-    # 実行結果の取得
-    res_info = json.loads(res.text)
-    jobid = res_info['id']
-    state = res_info['state']
-    return jobid, state
+    def delete_queryjob(self):
+        authorization = 'Bearer ' + self.access_token
+        headers = {'content-type': '',
+                   'Authorization': authorization
+                   }
+        url_queryjob = self.instance_url + "/services/data/" + self.version_number + "/jobs/query/" + self.jobid
 
+        res = requests.delete(url_queryjob, headers=headers)
+        # 実行結果を判定
+        if res.status_code != 204:
+            raise Exception(f'code:{res.status_code}, text:{res.text}')
 
-def get_oauth():
-    # --------------------------------------------------
-    # configparserの宣言とiniファイルの読み込み
-    # --------------------------------------------------
-    config_ini = configparser.ConfigParser()
-    config_ini.read('config.ini', encoding='utf-8')
+        # 実行結果の取得
+        res_info = res.status_code
+        return res_info
 
-    my_domain = config_ini['DEFAULT']['my_domain']
-    url_auth = "https://" + my_domain + ".my.salesforce.com/services/oauth2/token"
-    client_id = config_ini['DEFAULT']['client_id']
-    client_secret = config_ini['DEFAULT']['client_secret']
-    username = config_ini['DEFAULT']['username']
-    pw = config_ini['DEFAULT']['pw']
-    
-    # SalesForceへのセッション情報を取得
-    headers = {'content-type': 'application/x-www-form-urlencoded'}
-    payload = {
-        "grant_type": "password",
-        "client_id": client_id,
-        "client_secret": client_secret,
-        "username": username,
-        "password": pw
-    }
-    res = requests.post(url_auth, headers=headers, data=payload)
-    
-    # 実行結果を判定
-    if res.status_code != 200:
-        raise Exception(f'code:{res.status_code}, text:{res.text}')
-    
-    # 実行結果の取得
-    res_info = json.loads(res.text)
-    instance_url = res_info['instance_url']
-    access_token = res_info['access_token']
-    return instance_url, access_token
+    def get_queryresults(self):
+        authorization = 'Bearer ' + self.access_token
+        headers = {'Accept': 'text/csv',
+                   'Authorization': authorization
+                   }
+        url_queryjob \
+            = self.instance_url + "/services/data/" + self.version_number + "/jobs/query/" + self.jobid + "/results"
+        res = requests.get(url_queryjob, headers=headers)
+        res.encoding = res.apparent_encoding
+        # 実行結果を判定
+        if res.status_code != 200:
+            raise Exception(f'code:{res.status_code}, text:{res.text}')
+
+        # 実行結果の取得
+        res_info = res.text
+        self.results = res_info
+
+    def get_querybob(self):
+        authorization = 'Bearer ' + self.access_token
+        headers = {'content-type': 'application/json',
+                   'Authorization': authorization
+                   }
+        url_queryjob = self.instance_url + "/services/data/" + self.version_number + "/jobs/query/" + self.jobid
+
+        res = requests.get(url_queryjob, headers=headers)
+        # 実行結果を判定
+        if res.status_code != 200:
+            raise Exception(f'code:{res.status_code}, text:{res.text}')
+
+        # 実行結果の取得
+        res_info = json.loads(res.text)
+        state = res_info['state']
+        return state
+
+    def post_job(self):
+        authorization = 'Bearer ' + self.access_token
+        headers = {'content-type': 'application/json',
+                   'Authorization': authorization
+                   }
+        payload = {
+            "operation": "query",
+            "query": self.soql,
+            "contentType": "CSV",
+            "columnDelimiter": "COMMA",
+            "lineEnding": "CRLF"
+        }
+        url_query = self.instance_url + "/services/data/" + self.version_number + "/jobs/query"
+
+        res = requests.post(url_query, headers=headers, data=json.dumps(payload))
+        # 実行結果を判定
+        if res.status_code != 200:
+            raise Exception(f'code:{res.status_code}, text:{res.text}')
+
+        # 実行結果の取得
+        res_info = json.loads(res.text)
+        self.jobid = res_info['id']
+        state = res_info['state']
+        return state
 
 
 if __name__ == "__main__":
